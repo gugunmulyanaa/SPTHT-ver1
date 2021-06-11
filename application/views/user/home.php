85a9b857d2bbc570fb23841935fea50074adfbd8
gugunmulyanaa <gugunmulyana7@gmail.com>
2021-06-11 14:02:57 +0700

    app coba

---
+++ application/views/user/home.php
@@ -0,0 +1,130 @@
+       				<div class="container">
+					<div class="row justify-content-end fullscreen align-items-center">
+						<div class="col-lg-6 col-md-12 banner-left">
+								<h1 class="text-white">
+									<br>
+									Sistem Pakar Hama Dan Penyakit Tanaman Bonsai						
+								</h1>
+								<p class="mx-auto text-white  mt-20 mb-40">
+									Diagnosis Hama Dan Penyakit Pada Tanaman Bonsai
+								</p>
+						</div>
+						<div class="col-lg-6 col-md-12 no-padding banner-right">
+							<img style="height: 570px; width: 700px; margin-bottom: 30px;" src="<?php echo base_url() ?>assets/templateuser/img/bonsai11.png" alt="">
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->	
+			
+
+			<!-- Start service Area -->
+			<section class="service-area section-gap" id="feature" style="background-color: #F9F9FF;">
+				<div class="container">
+					<div class="row d-flex justify-content-center" ">
+						<div class="menu-content pb-60 col-lg-8">
+							<div class="title text-center">
+								<h1 class="mb-10">Informasi Penyakit</h1>
+								<p>Lihat beberapa informasi seputar penyakit pada tanaman Bonsai.</p>
+							</div>
+						</div>
+					</div>						
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-lg-6  col-md-6 mb-30">
+								<div class="single-service" style="background-color: white; padding: 20px;">
+									<img class="d-block mx-auto mb-30" src="<?php echo base_url() ?>assets/templateuser/img/11.png" alt="">
+									<h4><a href="#">Kutu Daun</a></h4>	
+									<span>Kutu daun adalah spesies serangga pemakan getah tanaman. Kutu daun hidup secara berkelompok (koloni), berwarna hitam, coklat atau hijau. Kutu daun berukuran kecil dan panjangnya antara 1 mm sampai 2 mm.</span>							
+								</div>
+							</div>
+							<div class="col-lg-6  col-md-6 mb-30">
+								<div class="single-service" style="background-color: white; padding: 20px;">
+									<img class="d-block mx-auto mb-30" src="<?php echo base_url() ?>assets/templateuser/img/12.png" alt="">
+									<h4><a href="#">Ulat</a></h4>
+									<span>Ulat merupakan salah satu fase dari metamorfosis serangga. Pada fase tersebut ulat akan memakan daun tanaman.Daun yang sudah dimakan ulat akan menganggu proses fotosintesis. Sehingga tanaman menjadi kuning, kurus dan mati</span>								
+								</div>
+							</div>
+							<div class="col-lg-6  col-md-6 mb-30">
+								<div class="single-service" style="background-color: white; padding: 20px;">
+									<img class="d-block mx-auto mb-30" src="<?php echo base_url() ?>assets/templateuser/img/13.png" alt="">
+									<h4><a href="#">Thrips</a></h4>		
+									<span>Thrips memiliki panjang sekitar 1-1,2 cm, berwarna hitam dengan garis merah pada bagian tubuhnya. Pada fase nimfa, thrips ini memiliki warna putih kekuningan dan tidak memiliki sayap. Sementara thrips dewasa atau imago memiliki sayap dan juga rambut yang berumbai-rumbai.</span>						
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-6 ">
+								<div class="single-service" style="background-color: white; padding: 20px;">
+									<img class="d-block mx-auto mb-30" src="<?php echo base_url() ?>assets/templateuser/img/14.png" alt="">
+									<h4><a href="#">Cabuk Putih</a></h4>	
+									<span>Cabuk putih merupakan hama yang bersifat partenogenetik telitoki, yaitu semua keturunan yang dihasilkan adalah betina, sehingga setiap kutu mampu manghasilkan keturunan. Pada kondisi optimal, satu betina mampu menghasilkan 200−600 butir telur. Cabuk putih dewasa memiliki tubuh berwarna merah muda dengan bentuk oval, ditutupi tepung putih berlilin, bagian mata relatif berkembang, dan tungkai berkembang baik dengan ukuran yang sama.</span>							
+								</div>
+							
+							</div>
+							
+							</div>		
+						</div>																	
+					</div>
+				</div>	
+			</section>
+			<!-- End service Area -->
+			
+			<!-- Start call-to-action Area -->
+			<section id="tentang" class="call-to-action-area section-gap" >
+				<div class="container">
+					<div class="row justify-content-center top">
+						<div class="col-lg-12">
+							<h1 class="text-white text-center">Tentang Kami!</h1><br><br><br>
+							<div class="col-lg-6" style="float: left;">
+								<img src="<?php echo base_url() ?>assets/templateuser/img/bonsai1.jpg"alt=".."style="width: 500px; height: 400px;">
+							</div>	
+							<div class="col-lg-6" style="float:right">
+								<p class="text-white mt-30" style="margin-top: 0" >
+									<span style="font-size: 28px; font-style: bold">SPBONSAI</span> <br>
+									SPBONSAI Merupakan Aplikasi Sistem Pakar Diagnosa Hama dan Penyakit Pada Tanaman Bonsai.
+									Aplikasi ini Bertujuan Untuk Memudahkan Para Petani Bonsai Mendiagnosis Penyakit Yang Menyerang Tanaman Bonsai Tsb.
+									
+								
+								</p>	
+								
+							</div>		
+									
+						</div>
+						
+					</div>
+					
+				</div>	
+			</section>
+			<!-- <div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pb-60 col-lg-8">
+							<div class="title text-center">
+								<h1 class="mb-10">Latest News from all categories</h1>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> labore  et dolore magna aliqua.</p>
+							</div>
+						</div>
+					</div> -->		
+
+			<section id="kontak" class="call-to-action-area section-gap " style="background-image: none;" >
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pb-60 col-lg-8">
+							<div class="title text-center">
+								<h1 class="mb-10">Get It Touch!</h1>
+								<p>Punya pertanyaan, saran atau kritikan? Hubungi kami atau kirim email kepada kami dan kami akan memberi tanggapan sesegera mungkin!</p>
+							</div>
+						</div>
+
+						<div class="col-md-8 col-md-offset-2" >
+							<form action="#" method="post" class="contact-form" >
+								<input type="text" class="name input" name="name" placeholder="Nama Depan" required="" style="background: none; border:1px solid #10161A; font-size: 14px;">
+								<input type="text" class="name input" name="name" placeholder="Nama Belakang" required="" style="background: none; border:1px solid #10161A; font-size: 14px;">
+								<input type="email" class="name input" name="name" placeholder="Email" required="" style="background: none; border:1px solid #10161A; font-size: 14px;">
+								<input type="text" class="name input" name="name" placeholder="Subject" required="" style="background: none; border:1px solid #10161A; font-size: 14px;">
+								<textarea placeholder="Pesan Anda" required="" style="background: none; border:1px solid #10161A; font-size: 14px;"></textarea>
+								<input type="submit" class="main-btn" style="background-color: #41C1FF" value="Kirim Pesan">
+							</form>
+						</div>							
+					</div>
+					
+				</div>	
+			</section>
